236a10cfc3f710c8f1ae5f88dd0cf1099092679c
RocketBlaster <47755918+Rocketblaster247@users.noreply.github.com>
2019-07-13 16:18:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-//Add Later
+var dataset = [];
+var load = function () {
+  
+};
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "jpn.txt");
+xhr.addEventListener("load", function (x) {
+  console.log("loaded en-ja dataset");
+  console.log(this.responseText);
+  load(x);
+});
