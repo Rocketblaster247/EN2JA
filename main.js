377b2b960c8869a2b5b1aa0ff83f2502ec4e61bf
RocketBlaster <47755918+Rocketblaster247@users.noreply.github.com>
2019-07-13 16:37:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 var dataset = [];
 var load = function (d) {
+    console.log("Finding sentences...");
     var sents = d.toString().split("\n");
-    for (var i = 0; i < sents.length; i ++) {
-        sents[i].toString().split("\t");
-        dataset[i] = [sents[i][0], sents[i][1]];
+    console.log("Loaded dataset...");
+    console.log(dataset);
+    if (!brain) {
+        console.log("Missing dependencie 'brain.js'");
+        return;
     }
+    console.log("Preparing to train...");
 };
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "jpn.txt");
 xhr.addEventListener("load", function (x) {
-    console.log("loaded en-ja dataset");
-    load(this.responseText);
+    console.log("Loaded file...");
 });
 xhr.send();
