b1c99b3d1bfa42c3f779d64c36601d9d3b931d2c
RocketBlaster <47755918+Rocketblaster247@users.noreply.github.com>
2019-07-13 16:50:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 var dataset = [];
+var net = new brain.recurrent.LSTM();
+
 var load = function (d) {
     console.log("Finding sentences...");
-    dataset = d.toString().split("\n");
+    dataset = d.toString().split("\t").join("   ").toString().split("\n");
     console.log("Loaded dataset...");
     console.log(dataset);
     if (!brain) {
         console.log("Missing dependencie 'brain.js'");
         return;
     }
-    console.log("Preparing to train...");
+    console.log("Training...");
+    net.train(dataset, {
+        iterations: 20000,
+        errorThresh: 0.005,
+        log: true,
+        logPeriod: 10,
+        learningRate: 0.3,
+        momentum: 0.1,
+        callback: null,
+        callbackPeriod: 10,
+        timeout: Infinity,
+    });
 };
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "jpn.txt");
