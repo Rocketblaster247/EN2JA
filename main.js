c76e43f5c4e409bb797272d68030cf6ec1026349
RocketBlaster <47755918+Rocketblaster247@users.noreply.github.com>
2019-07-15 18:16:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ var load = function (d) {
         var x = iter;
         var y = iter + parseInt(window.prompt("Training Size:"));
         iter = y;
-        net.train(dataset.slice(x, y), {
+        net.train(dataset[0].toString().slice(x, y), {
             iterations: 300,
             errorThresh: 0.02,
             log: true,
